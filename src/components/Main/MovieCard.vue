3b920736ff4cc89df2a7ca724d9a80803878a197
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-12-01 15:55:30 +0100

    dynamic star rating done

--- src/components/Main/MovieCard.vue
+++ src/components/Main/MovieCard.vue
@@ -7,12 +7,12 @@
               <img :src="details.language === 'it' ? require('@/assets/img/ita.png') : require('@/assets/img/eng.png')" alt="">
               <!-- {{ details.language }} -->
           </li> 
-          <li>{{ details.rating }}</li>
-          <font-awesome-icon :icon="['far', 'star']"/>
-          <font-awesome-icon :icon="['far', 'star']"/>
-          <font-awesome-icon :icon="['far', 'star']"/>
-          <font-awesome-icon :icon="['far', 'star']"/>
-          <font-awesome-icon :icon="['far', 'star']"/>
+          <li>{{ ratingOutOfFive }}</li>
+          <font-awesome-icon v-for="n in 5" :key="n" :id="n" :icon="['fas', 'star']" :class="n <= ratingOutOfFive ? 'rated' : 'not-rated'"/>
+          <!-- <font-awesome-icon :icon="['fas', 'star']"/>
+          <font-awesome-icon :icon="['fas', 'star']"/>
+          <font-awesome-icon :icon="['fas', 'star']"/>
+          <font-awesome-icon :icon="['fas', 'star']"/> -->
           <li>
               <img :src="details.poster" :alt="details.title">
         </li>
@@ -27,9 +27,24 @@ export default {
   props: {
       details: Object
   },
+  computed: {
+      ratingOutOfFive() {
+          if (this.details.rating) {
+              return Math.floor((5 * this.details.rating) / 10);
+          } else {
+              return 'Voto non disponibile';
+          }
+      }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+    .rated {
+        color: blue;
+    }
 
+    .not-rated {
+        color: yellow;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faStar);
