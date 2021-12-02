03c3cb7e9823d9753f31efac3287aa53296f342a
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-12-02 13:13:10 +0100

    no overview edge case handled

--- src/components/Main/MovieCard.vue
+++ src/components/Main/MovieCard.vue
@@ -4,18 +4,19 @@
           <ul class="details">
             <li><strong>Titolo originale: </strong>{{ details.originalTitle }}</li>
             <li><strong>Titolo: </strong>{{ details.title }}</li>
-            <li><strong>Overview: </strong>{{ details.overview }}</li>
             <li><strong>Lingua: </strong>
                 <img v-if="details.language === 'it' || details.language === 'en'" class="language" :src="require('@/assets/img/' + details.language + '.png')" :alt="details.language">
                 <span v-else>{{ details.language.toUpperCase() }}</span>               
             </li> 
-            <li><strong>Voti: </strong>
+            <li><strong>Voto: </strong>
                 <span v-if="ratingOutOfMaxRating">{{ ratingOutOfMaxRating }}/{{maxRating}}</span>
                 <span v-else>Voto non disponibile</span>
                 <div>
                     <font-awesome-icon v-for="n in maxRating" :key="n" :id="n" :icon="['fas', 'star']" :class="n <= ratingOutOfMaxRating ? 'rated' : 'not-rated'"/>
                 </div>
-            </li>              
+            </li>
+            <li v-if="details.overview"><strong>Overview: </strong>{{ details.overview }}</li> 
+            <li v-else><strong>Overview: </strong>Overview non disponibile</li>           
           </ul>
           <div class="poster">
             <img :src="details.poster" :alt="details.title">              
